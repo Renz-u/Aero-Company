1e4d0b48776637087a522e0d8c2b8b2d89d23040
Renz-u <anthonygeronga@gmail.com>
2025-12-16 22:35:13 +0800

    improved products

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Aero website loaded successfully.");
+ function openModal(name, price, capacity) {
+      document.getElementById('shipName').innerText = name;
+      document.getElementById('shipInfo').innerText = `Price: $${price.toLocaleString()} | Capacity: ${capacity}`;
+      document.getElementById('modal').style.display = 'flex';
+    }
+
+    function closeModal() {
+      document.getElementById('modal').style.display = 'none';
+    }
+
