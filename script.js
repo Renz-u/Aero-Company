4d92eef670740d5ae0eea99f209ad603adb044e3
Renz-u <anthonygeronga@gmail.com>
2025-12-18 18:47:57 +0800

    edit kevs

--- script.js
+++ script.js
@@ -8,3 +8,38 @@
       document.getElementById('modal').style.display = 'none';
     }
 
+  const items = document.querySelectorAll('.gallery-item');
+  const buttons = document.querySelectorAll('.filter-btn');
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      buttons.forEach(b => b.classList.remove('active'));
+      btn.classList.add('active');
+
+      const filter = btn.dataset.filter;
+
+      items.forEach(item => {
+        item.style.display =
+          filter === 'all' || item.classList.contains(filter)
+            ? 'block'
+            : 'none';
+      });
+    });
+  });
+
+  // IMAGE MODAL
+  const modal = document.getElementById('imageModal');
+  const modalImg = document.getElementById('modalImg');
+  const modalText = document.getElementById('modalText');
+
+  items.forEach(item => {
+    item.addEventListener('click', () => {
+      modal.style.display = 'flex';
+      modalImg.src = item.querySelector('img').src;
+      modalText.textContent = item.querySelector('.overlay').textContent;
+    });
+  });
+
+  document.querySelector('.close-modal').onclick = () => {
+    modal.style.display = 'none';
+  };
